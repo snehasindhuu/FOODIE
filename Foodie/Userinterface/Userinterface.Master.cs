6093f89b6c3da5fd5e8b8cc0e0d8282c3c92eac7
snehasindhuu <150651265+snehasindhuu@users.noreply.github.com>
2024-03-24 14:39:03 +0530

    Online Food Ordering
    Added Online Food Ordering

---
+++ Foodie/Userinterface/Userinterface.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Foodie.Userinterface
+{
+    public partial class Userinterface : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                //Load the control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+                //Add the control to the panel
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
